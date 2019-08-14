3dda3526c477c513c471d66641bf6ce0b3cd4f7a
Syoji Kinoshita <kinoshita0517@gmail.com>
2019-08-14 23:25:52 +0900

    2019/08/14 start gamen sakusei i#

--- workspace/symphogear-sim/src/views/Home.vue
+++ workspace/symphogear-sim/src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <StartScreen v-if="showStart"/>
+    <NormalScreen v-if="showNormal"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import StartScreen from '@/components/StartScreen.vue'
+import NormalScreen from '@/components/NormalScreen.vue'
 
 export default {
   name: 'home',
+  data () {
+    return {
+      showStart: true,
+      showNormal: false
+    }
+  },
   components: {
-    HelloWorld
+    StartScreen,
+    NormalScreen
+  },
+  mounted () {
+    this.showStart = true
+    this.showNormal = false
+  },
+  computed: {
+    // Storeを監視
+    nowScreenId: function () {
+      return this.$store.state.screenId
+    }
+  },
+  watch: {
+    // Store.screenIdが変更された場合、画面表示を切り替える
+    nowScreenId: function (id) {
+      switch (id) {
+        case 1:
+          this.showStart = false
+          this.showNormal = true
+          break
+        default:
+          this.showStartScreen = true
+          this.showNormal = false
+          // ScreenIdが該当しない場合、開始画面を表示する
+          this.$store.commit('saveScreenName', 0)
+      }
+    }
   }
+
 }
 </script>
