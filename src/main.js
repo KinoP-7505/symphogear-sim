d75d4ecf93dc094b7ad9fb9e95f149fa33696269
Syoji Kinoshita <kinoshita0517@gmail.com>
2019-11-19 23:37:14 +0900

    2019/11/19 OnsenUIテスト

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import VueOnsen from 'vue-onsenui'; // This already imports 'onsenui'
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  VueOnsen,
   render: h => h(App)
 }).$mount('#app')
